c1cf6c724d37a9f0235517b10ed502a7ed4f54ff
Bryan Cendales <bn.cendales10@gmail.com>
2026-02-03 21:30:03 +0900

    Update: Styling to name cards, with delete and edit functionality

--- src/components/optionsList.component.jsx
+++ src/components/optionsList.component.jsx
@@ -1,12 +1,56 @@
 import "./styles/optionsList.styles.scss";
+import { Fragment, useState } from "react";
+import { ReactComponent as EditIcon } from "../images/icons/edit-icon.svg";
+import { ReactComponent as DeleteIcon } from "../images/icons/trash-bin.svg";
 
-const OptionsList = ({ optionsArray, onRemove }) => {
+const OptionsList = ({ optionsArray, onRemove, onEdit }) => {
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [tempEditValue, setTempEditValue] = useState("");
+
   return (
     <div className="options-list">
       {optionsArray.map((option, index) => (
         <div key={index} className="option-item">
-          <span>{option.option}</span>
-          {onRemove && <button onClick={() => onRemove(index)}>X</button>}
+          {editingIndex === index ? (
+            <input
+              type="text"
+              value={tempEditValue}
+              maxLength={10}
+              onChange={(e) => {
+                const newValue = e.target.value;
+                if (newValue.length <= 10) {
+                  setTempEditValue(newValue);
+                }
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  onEdit(index, tempEditValue);
+                  setEditingIndex(null);
+                }
+              }}
+              autoFocus
+            />
+          ) : (
+            <Fragment>
+              <span>{option.option}</span>
+              {onEdit && (
+                <button
+                  className="edit-btn"
+                  onClick={() => {
+                    setEditingIndex(index);
+                    setTempEditValue(option.option);
+                  }}
+                >
+                  <EditIcon style={{ width: "20px", height: "20px" }} />
+                </button>
+              )}
+              {onRemove && (
+                <button className="delete-btn" onClick={() => onRemove(index)}>
+                  <DeleteIcon style={{ width: "20px", height: "20px" }} />
+                </button>
+              )}
+            </Fragment>
+          )}
         </div>
       ))}
     </div>
