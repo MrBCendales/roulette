0d9c461fbeb7f37bc386864d35f862fc8b190d31
Bryan Cendales <bn.cendales10@gmail.com>
2026-02-24 11:30:23 +0900

    Feat: Changed structure to change the direction of the project. New folders created

---
+++ src/components/home/ProfileCard.component.jsx
@@ -0,0 +1,28 @@
+import { STRINGS_EN } from "../../locales/en";
+import "./ProfileCard.styles.scss";
+
+const ProfileCard = () => {
+  const STRINGS = STRINGS_EN.HOME.PROFILE;
+
+  return (
+    <section className="profile-card">
+      <div className="profile-photo">
+        <img src={STRINGS.PHOTO_URL} alt="Bryan Cendales" />
+      </div>
+
+      <h3 className="profile-name">{STRINGS.NAME}</h3>
+
+      <div className="social-links">{STRINGS.SOCIAL_LINKS}</div>
+
+      <div className="skills-grid">
+        {STRINGS.SKILLS.map((skill, index) => (
+          <span key={index} className="skill-badge">
+            {skill}
+          </span>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ProfileCard;
