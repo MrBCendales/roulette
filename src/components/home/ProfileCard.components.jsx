f2c8c723643c0ffdfcef93183bef4364edbdf9c8
MrBCendales <bn.cendales10@gmail.com>
2026-02-27 23:48:35 +0900

    Profile card updated

--- src/components/home/ProfileCard.components.jsx
+++ src/components/home/ProfileCard.components.jsx
@@ -1,5 +1,6 @@
 import { STRINGS_EN } from "../../locales/en";
 import "./ProfileCard.styles.scss";
+import { Link } from "react-router-dom";
 
 const ProfileCard = () => {
   const STRINGS = STRINGS_EN.HOME.PROFILE;
@@ -9,22 +10,10 @@ const ProfileCard = () => {
       <div className="profile-photo">
         <img src={STRINGS.PHOTO_URL} alt="Bryan Cendales" />
       </div>
-      
+
       <h3 className="profile-name">{STRINGS.NAME}</h3>
-      
-      <div className="social-links">
-        {STRINGS.SOCIAL_LINKS.map((link, index) => (
-          
-            key={index}
-            href={link.URL}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="social-link"
-          >
-            {link.NAME}
-          
-        ))}
-      </div>
+
+      <div className="social-links">{STRINGS.SOCIAL_LINKS}</div>
 
       <div className="skills-grid">
         {STRINGS.SKILLS.map((skill, index) => (
